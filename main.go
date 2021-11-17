25a6fdd4352ccf9d302aa150601f289e7507d827
Rodolphe Blancho <rodolphe.blancho@gmail.com>
2021-11-17 17:28:59 +1100

    feat: add option to fail on violation error

--- main.go
+++ main.go
@@ -12,15 +12,17 @@ import (
 )
 
 var (
-	dir string
-	reportfile string
+	dir              string
+	reportfile       string
 	minErrorPriority int
+	failOnError bool
 )
 
 func main() {
 	flag.StringVar(&dir, "dir", "", "")
 	flag.StringVar(&reportfile, "reportfile", "", "")
-	flag.IntVar(&minErrorPriority, "min-error-priority", 1, "")
+	flag.IntVar(&minErrorPriority, "min-error-priority", 0, "")
+	flag.BoolVar(&failOnError, "fail-on-error", false, "")
 
 	flag.Parse()
 
@@ -34,19 +36,24 @@ func main() {
 	}
 
 	violationCount := 0
+	warningCount := 0
+	errorCount := 0
 	for _, f := range report.Files {
 		relPath, err := relpath(f.Filename)
 		if err != nil {
 			githubactions.Fatalf("could not get path of file ''%s': %s", f.Filename, err)
 		}
 
-		githubactions.Group(fmt.Sprintf("PMD %s: errors=%d", relPath, len(f.Violations)))
+		githubactions.Group(fmt.Sprintf("PMD %s: violations=%d", relPath, len(f.Violations)))
 		for _, v := range f.Violations {
 			violationCount += 1
-			annotation := fmt.Sprintf("file=%s,line=%d::[%s] %s", relPath, v.BeginLine, v.Rule, v.Description)
-			if (v.Priority > minErrorPriority) {
+			annotation := fmt.Sprintf("file=%s,line=%d,title=%s::%s", relPath, v.BeginLine, v.Rule, v.Description)
+
+			if v.Priority > minErrorPriority {
+				warningCount += 1
 				githubactions.Warningf(annotation)
 			} else {
+				errorCount += 1
 				githubactions.Errorf(annotation)
 			}
 		}
@@ -56,12 +63,15 @@ func main() {
 	if violationCount == 0 {
 		githubactions.Infof("no problem found")
 	} else {
-		githubactions.Infof("%d problem(s)", violationCount)
+		githubactions.Infof("%d violations(s), errors=%d, warnings=%d", violationCount, errorCount, warningCount)
+		if errorCount > 0 && failOnError {
+			os.Exit(4)
+		}
 	}
 }
 
 func relpath(file string) (string, error) {
-	cwd, err := os.Getwd();
+	cwd, err := os.Getwd()
 	if err != nil {
 		return "", err
 	}
